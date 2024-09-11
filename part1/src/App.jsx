eb9bb813fd5c773d9a98b22b2e6581e9cef225a0
Sam Ly <sam@samly.me>
2024-09-11 00:50:04 -0700

    e 1.4 1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,57 +1,24 @@
-const Header = ({ course }) => {
-  return (
-    <h1>{course}</h1>
-  )
-}
+import { useState } from "react"
 
-const Content = ({ parts }) => {
-  return (
-    <div className="content">
-      {
-        parts.map(
-          (elem, index) => <Part name={elem.name} exercises={elem.exercises} key={index} />
-        )
-      }
-    </div>
-  )
-}
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>{name} {exercises}</p>
-  )
-}
-
-const Total = ({ parts }) => {
-  return (
-    <p>Number of execises {parts.reduce((elem)=>elem, 0)}</p>
-  )
-}
 const App = () => {
-  const course = 'Half Stack application development'
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
   
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <Header course={course}/> 
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <div>{counter}</div>
+
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+
+      <button onClick={setToZero}>
+        zero
+      </button>
     </div>
   )
 }
-
 export default App
