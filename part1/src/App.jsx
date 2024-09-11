4f6ac7a7d3682a43b0ef68b26eba98a5a0844767
Sam Ly <sam@samly.me>
2024-09-11 01:05:35 -0700

    e 1.8 unicafe step 3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react"
 
+const Statistics = ({ good, neutral, bad }) => {
+  const average = (good, neutral, bad) => (good - bad)/(good + neutral + bad)
+  const positive = (good, neutral, bad) => 100*good/(good + neutral + bad)
+
+  return (
+    <div className="statistics">
+      <p>average {average(good, neutral, bad)}</p>
+      <p>positive {positive(good, neutral, bad)}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const average = (good, neutral, bad) => (good - bad)/(good + neutral + bad)
-  const positive = (good, neutral, bad) => good/(good + neutral + bad)
 
   return (
     <div className="app">
@@ -21,8 +30,7 @@ const App = () => {
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {good + neutral + bad}</p>
-      { good + neutral + bad > 0 && <p>average {average(good, neutral, bad)}</p> }
-      { good + neutral + bad > 0 && <p>positive {positive(good, neutral, bad)}%</p> }
+      { good + neutral + bad > 0 && <Statistics good={good} neutral={neutral} bad={bad} /> }
     </div>
   )
 }
