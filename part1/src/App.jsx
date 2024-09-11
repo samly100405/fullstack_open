3e3671204e6004db89efba934939b662abb38635
Sam Ly <sam@samly.me>
2024-09-11 00:55:10 -0700

    e 1.6 unicafe step 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,23 +1,22 @@
 import { useState } from "react"
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  const increaseByOne = () => setCounter(counter + 1)
-  
-  const setToZero = () => setCounter(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      <div>{counter}</div>
+    <div className="app">
+      <h1>give feedback</h1>
 
-      <button onClick={increaseByOne}>
-        plus
-      </button>
+      <button onClick={() => setGood(good+1)}>good</button>
+      <button onClick={() => setNeutral(neutral+1)}>neutral</button>
+      <button onClick={() => setBad(bad+1)}>bad</button>
 
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
