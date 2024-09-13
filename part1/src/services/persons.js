18486dcb3a80bfaa3543f187b6c00d77e557c444
Sam Ly <sam@samly.me>
2024-09-13 16:09:18 -0700

    extract db services

---
+++ part1/src/services/persons.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+    return axios
+            .get(baseUrl)
+            .then(
+                (res) => {
+                    return res.data
+                }
+            )
+}
+
+const create = (newPerson) => {
+    return axios
+            .post(baseUrl, newPerson)
+            .then(
+                (res) => {
+                    return res.data
+                }
+            )
+}
+
+const deletePerson = (personID) => {
+    return axios
+            .delete(`${baseUrl}/${personID}`)
+            .then(
+                (res) => {
+                    return res.data
+                }
+            )
+}
+
+export default {
+    getAll,
+    create,
+    deletePerson,
+}
