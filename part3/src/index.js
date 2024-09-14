2ac712dcc08b8746feca7f54e35018c9d0b61020
Sam Ly <sam@samly.me>
2024-09-14 11:41:43 -0700

    e 3.4 3.5

--- part3/src/index.js
+++ part3/src/index.js
@@ -1,28 +1,29 @@
 import express, { response } from 'express'
 
 const app = express()
+app.use(express.json())
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    "id": "1",
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": "2",
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": "3",
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": "4",
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
 ]
 
 app.get('/', (request, response) => {
@@ -30,7 +31,7 @@ app.get('/', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    response.send(`
+  response.send(`
         Phonebook has info for ${persons.length} people. <br/>
         ${Date()}`)
 })
@@ -39,27 +40,42 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
-app.get('/api/persons/:personID', (request, response) => {
-    const personID = request.params.personID
-    const person = persons.find((elem) => elem.id === personID)
+app.post('/api/persons', (request, response) => {
+  if (!request.body.name || !request.body.number) {
+    return response.status(400).json({ error: "missing name or number" })
+  }
 
-    if (!person) {
-        return response.status(404).json({error: 'person not found'})
+  persons.push(
+    {
+      id: Math.random().toString(16).slice(2), 
+      name: request.body.name, 
+      number: request.body.number
     }
+  )
+  return response.json(persons)
+})
 
-    return response.json(person)
+app.get('/api/persons/:personID', (request, response) => {
+  const personID = request.params.personID
+  const person = persons.find((elem) => elem.id === personID)
+
+  if (!person) {
+    return response.status(404).json({ error: 'person not found' })
+  }
+
+  return response.json(person)
 })
 
 app.delete('/api/persons/:personID', (request, response) => {
-    const personID = request.params.personID
-    const person = persons.findIndex((elem) => elem.id === personID)
+  const personID = request.params.personID
+  const person = persons.findIndex((elem) => elem.id === personID)
 
-    if (person == -1) {
-        return response.status(404).json({error: 'person not found'})
-    }
+  if (person == -1) {
+    return response.status(404).json({ error: 'person not found' })
+  }
 
-    persons.splice(person, 1)
-    return response.json(persons)
+  persons.splice(person, 1)
+  return response.json(persons)
 })
 
 const PORT = 3000
